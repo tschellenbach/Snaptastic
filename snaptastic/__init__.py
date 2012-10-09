4d766d2db53177e24b5a177bc779033e4decd62e
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-10-09 14:11:32 +0200

    ok now it might work

--- snaptastic/__init__.py
+++ snaptastic/__init__.py
@@ -1,44 +1,25 @@
-'''
+import sys
 
-Snapshotter class
-- instantiate with context, metadata and connection
-- pre snapshots hook
-- pre snapshot hook
-- post snapshot hook
-- post snapshots hook
-- get_snapshot_for_mount_point function
+__author__ = 'Thierry Schellenbach'
+__copyright__ = 'Copyright 2012, Thierry Schellenbach'
+__credits__ = [
+    'Mike Ryan, Thierry Schellenbach, mellowmorning.com, @tschellenbach']
+__license__ = 'BSD'
+__version__ = '0.0.1'
+__maintainer__ = 'Thierry Schellenbach'
+__email__ = 'thierryschellenbach@gmail.com'
+__status__ = 'Production'
 
+setup_install = 'setup.py' in sys.argv and 'install' in sys.argv
 
-Volume class
-- device
-- instance device
-- size
-- mount_point
-- mount function
-- attach function
-- get_latest_snapshot function
-
-Freeze context manager
-- good tutorial
-http://www.doughellmann.com/PyMOTW/contextlib/
-
-'''
-
-# wark around to allow setup.py to import from snaptastic.meta instead of here
-from snaptastic.meta import __author__, __copyright__, __credits__, __license__
-from snaptastic.meta import __version__, __maintainer__, __email__, __status__
-
-
-from snaptastic.utils import get_ec2_conn
-from snapshotter import Snapshotter
-from ebs_volume import EBSVolume
-from snaptastic import settings
-
-
-import logging.config
-logging.config.dictConfig(settings.LOGGING_CONFIG)
-
-snapshotters = {}
+if not setup_install:
+    from snaptastic.utils import get_ec2_conn
+    from snapshotter import Snapshotter
+    from ebs_volume import EBSVolume
+    from snaptastic import settings
+    import logging.config
+    logging.config.dictConfig(settings.LOGGING_CONFIG)
+    snapshotters = {}
 
 
 def register(snapshotter):
@@ -54,7 +35,8 @@ def get_snapshotter(snapshotter_name):
         raise ValueError(
             error_format % (snapshotter_name, snapshotters.keys()))
     return snapshotters[snapshotter_name]
+    
 
-
-#register the examples
-from examples import *
+if not setup_install:
+    #register the examples
+    from examples import *
--- snaptastic/meta.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Thierry Schellenbach'
-__copyright__ = 'Copyright 2012, Thierry Schellenbach'
-__credits__ = [
-    'Mike Ryan, Thierry Schellenbach, mellowmorning.com, @tschellenbach']
-__license__ = 'BSD'
-__version__ = '0.0.1'
-__maintainer__ = 'Thierry Schellenbach'
-__email__ = 'thierryschellenbach@gmail.com'
-__status__ = 'Production'
