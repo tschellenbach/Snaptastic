48eb373573a2e44eb8faf2be0689ff77abfb3135
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-10-09 14:19:36 +0200

    creating log dir if it isnt there

--- snaptastic/default_settings.py
+++ snaptastic/default_settings.py
@@ -1,5 +1,17 @@
 
 import os
+error_log_path = os.path.join('/var', 'log', 'snaptastic', 'error.log')
+log_path = os.path.join('/var', 'log', 'snaptastic', 'info.log')
+
+
+def ensure_dir(path):
+    path_dir, filename = os.path.split(path)
+    if not os.path.isdir(path_dir):
+        os.makedirs(path_dir)
+
+ensure_dir(error_log_path)
+ensure_dir(log_path)
+
 LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -16,12 +28,12 @@ LOGGING_CONFIG = {
         'error_file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
-            'filename': os.path.join('/var', 'log', 'snaptastic', 'error.log')
+            'filename': error_log_path
         },
         'file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
-            'filename': os.path.join('/var', 'log', 'snaptastic', 'info.log')
+            'filename': log_path
         }
 
     },
