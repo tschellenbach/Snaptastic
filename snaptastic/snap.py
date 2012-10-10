7a277c88722b5b1aa62307a38e6d8e4097517532
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-10-10 12:14:28 +0200

    added test command to validate boto lookups

--- snaptastic/snap.py
+++ snaptastic/snap.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import logging
+logger = logging.getLogger('snaptastic.cli')
 
 path = os.path.abspath(__file__)
 parent = os.path.join(path, '../', '../')
@@ -23,6 +25,34 @@ def mount_snapshots(snapshotter_name, verbosity=2):
     snap.mount_snapshots()
 
 
+@command
+def list_volumes(snapshotter_name, verbosity=2):
+    snapshotter_class = get_snapshotter(snapshotter_name)
+    snap = snapshotter_class()
+    volumes = snap.get_volumes()
+    for volume in volumes:
+        print volume
+
+
+@command
+def test(verbosity=2):
+    from snaptastic.utils import get_userdata_dict
+    logger.info('trying to get userdata, requires boto and valid keys')
+    try:
+        userdata = get_userdata_dict()
+        logger.info('found userdata, so that works %s', userdata)
+    except Exception, e:
+        logger.exception('Userdata lookup doesnt work, error %s', e)
+    logger.info('next up instance metadata')
+    from boto.utils import get_instance_metadata
+    try:
+        metadata = get_instance_metadata()
+        logger.info('found instance metadata %s', metadata)
+    except Exception, e:
+        logger.exception('Metadata lookup doesnt work, error %s', e)
+    
+    
+
 p = ArghParser()
-p.add_commands([make_snapshots, mount_snapshots])
+p.add_commands([make_snapshots, mount_snapshots, list_volumes, test])
 p.dispatch()
