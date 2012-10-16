48b9ce922571fd63a53c3e8d02d6a34cd1986ed6
Thierry Schellenbach <thierryschellenbach@gmail.com>
2012-10-16 14:27:43 +0200

    change

--- snaptastic/__init__.py
+++ snaptastic/__init__.py
@@ -5,7 +5,7 @@ __copyright__ = 'Copyright 2012, Thierry Schellenbach'
 __credits__ = [
     'Mike Ryan, Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 __license__ = 'BSD'
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
