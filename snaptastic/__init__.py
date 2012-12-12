1e130d7fbc8b1472daa412eb171979500ff1f19b
Peter van Kampen <pterk@datatailors.com>
2012-12-12 16:24:50 +0100

    expand tests, bump version

--- snaptastic/__init__.py
+++ snaptastic/__init__.py
@@ -5,7 +5,7 @@ __copyright__ = 'Copyright 2012, Thierry Schellenbach'
 __credits__ = [
     'Mike Ryan, Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 __license__ = 'BSD'
-__version__ = '0.1.6'
+__version__ = '0.2'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
